ff8cddb5e2f73ca3c110859023cca57a96247c97
Rameez Bhatti <xrkalix@gmail.com>
2024-02-13 18:53:37 +0500

    thing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use ECUApp\SharedCode\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $user = User::first();
+
+    dd($user);
+    // return view('welcome');
 });
