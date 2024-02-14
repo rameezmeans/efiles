36a3bf60233d50d9f6bfaae5f717435b306cd873
Rameez Bhatti <xrkalix@gmail.com>
2024-02-14 18:28:25 +0500

    thing

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -44,7 +44,9 @@ class AuthController extends Controller
         ]);
    
         $credentials = $request->only('email', 'password');
-        
+
+        dd(Auth::attempt($credentials));
+
         if (Auth::attempt($credentials)) {
             return redirect()->intended('home')
                         ->withSuccess('You have Successfully loggedin!');
