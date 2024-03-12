cd6c44a616757c03323260046e927e345a24b152
Rameez Bhatti <xrkalix@gmail.com>
2024-03-12 21:02:51 +0500

    files controller

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use ECUApp\SharedCode\Controllers\FilesMainController;
+use ECUApp\SharedCode\Models\File;
+use ECUApp\SharedCode\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FileController extends Controller
 {
@@ -12,13 +16,26 @@ class FileController extends Controller
      * @return void
      */
 
+    private $filesMainObj;
+
     public function __construct(){
 
         $this->middleware('auth', [ 'except' => [ 'feedbackLink' ] ]);
+        $this->filesMainObj = new FilesMainController();
 
     }
 
-    public function cart() {
-        
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function fileHistory()
+    {
+        $frontendID = 2;
+        $user = User::findOrFail(Auth::user()->id);
+        $files = $this->filesMainObj->getFiles($user, $frontendID);
+
+        return view('files.file_history', [ 'files' => $files, 'user' => $user ]);
     }
 }
