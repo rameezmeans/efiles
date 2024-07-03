8f87c2c4aad4098f894b93a4a7852be27e6d7b26
Rameez Bhatti <xrkalix@gmail.com>
2024-07-03 21:42:13 +0500

    auto download thing

--- resources/views/files/auto_download.blade.php
+++ resources/views/files/auto_download.blade.php
@@ -942,18 +942,37 @@ body  {
 <script src="https://js.pusher.com/7.0.3/pusher.min.js"></script>
 <script>
     // Enable pusher logging - don't include this in production
+    // Pusher.logToConsole = true;
+
+    // var pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
+    //     encrypted: true,
+    //     cluster: "{{ config('chatify.pusher.options.cluster') }}",
+    //     authEndpoint: '{{route("pusher.auth")}}',
+    //     auth: {
+    //         headers: {
+    //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //         }
+    //     }
+    // });
+
     Pusher.logToConsole = true;
 
-    var pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
-        encrypted: true,
-        cluster: "{{ config('chatify.pusher.options.cluster') }}",
-        authEndpoint: '{{route("pusher.auth")}}',
-        auth: {
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+    var key = '{{env('PUSHER_APP_KEY')}}';
+
+    console.log('key:'+key);
+
+    var pusher = new Pusher(key, {
+    encrypted: true,
+    cluster: "ap2",
+    authEndpoint: '{{route("pusher.auth")}}',
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
+    }
     });
+
+    console.log(pusher);
 
 </script>
 <script type="text/javascript">
