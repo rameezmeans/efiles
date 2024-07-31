ee5bfedd0a08bb4447d7f8ea3232ca0cec52cb2c
Rameez Bhatti <xrkalix@gmail.com>
2024-07-31 07:57:55 +0500

    goip resolved

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -75,19 +75,25 @@ class LoginController extends Controller
             return $this->sendLockoutResponse($request);
         }
 
-        if ($this->attemptLogin($request)) {
-            //check user is admin or not  
+        try{
+
+            if ($this->attemptLogin($request)) {
+                //check user is admin or not  
+                
+                if (Auth::user()->front_end_id == 3 && Auth::user()->subdealer_group_id == NULL && Auth::user()->is_admin() == false && Auth::user()->is_engineer() == false) {
+                    return $this->sendLoginResponse($request);
+                }
+                else if (Auth::user()->is_admin() == true) {
+                    return $this->sendLoginResponse($request);
+                }
+                else {
+                    $this->guard()->logout();
+                    // return $this->notAdmin($request);
+                }
+            }
+        }
+        catch(\Exception $e){
             
-            if (Auth::user()->front_end_id == 3 && Auth::user()->subdealer_group_id == NULL && Auth::user()->is_admin() == false && Auth::user()->is_engineer() == false) {
-                return $this->sendLoginResponse($request);
-            }
-            else if (Auth::user()->is_admin() == true) {
-                return $this->sendLoginResponse($request);
-            }
-            else {
-                $this->guard()->logout();
-                // return $this->notAdmin($request);
-            }
         }
 
         // If the login attempt was unsuccessful we will increment the number of attempts
