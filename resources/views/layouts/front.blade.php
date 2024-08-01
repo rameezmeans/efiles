38b929111eb41727c9d69554c3d7b79664edffb1
Rameez Bhatti <xrkalix@gmail.com>
2024-08-01 14:35:11 +0500

    google thing

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,6 +7,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W3ZKMF4C');</script>
+        <!-- End Google Tag Manager -->
+
     <title>{{ config('app.name', 'DEV | Tuning-X | Performance Excellence') }}</title>
 
     <!-- Fonts -->
@@ -75,6 +83,10 @@
 
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W3ZKMF4C"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     @yield('content')
     @yield('pagespecificscripts')
 </body>
