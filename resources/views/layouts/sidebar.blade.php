794333d174713a0874946bb1df2b45865bf32246
Rameez Bhatti <xrkalix@gmail.com>
2024-10-30 19:14:50 +0500

    file event note

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,14 +16,14 @@
 		<p style="font-size: 10px;">Mon-Fri: ({{ date('h:i A', strtotime($workHours[0]->start))}} - {{ date('h:i A', strtotime($workHours[0]->end)) }})</p>
 		<p style="font-size: 10px;">Sat: ({{ date('h:i A', strtotime($workHours[1]->start))}} - {{ date('h:i A', strtotime($workHours[1]->end)) }}) Sunday: (Closed)</p>
 		<span>{{__('File Service Status')}}:</span>
-		<p style="margin-top: 5px;"><span class="dot @if($feed->type == 'danger') dot-danger @else dot-success @endif""></span> @if($feed->type == 'danger') Offline @else Online @endif - <span id="MyClockDisplay"></span></p>
+		<p style="margin-top: 5px;"><span class="dot @if($feed->type == 'danger') dot-danger @else dot-success @endif""></span> @if($feed->type == 'danger') Offline @else Online @endif - <span id="MyClockDisplay"></span> (LOCAL Time)</p>
 		</div>
 	@endif
 
 	<div class="box @if($OnlineStatus == 0) box-danger @else box-success @endif" style="height: 80px !important;">
 		<p style="font-size: 10px;">24h / 7d</p>
 		
-		<span>{{__('Automatic File Service Status')}}:</span>
+		<p>{{__('Automatic File Service Status')}}:</p>
 		<p class="dot @if($OnlineStatus == 0) dot-danger @else dot-success @endif""><p>@if($OnlineStatus == 0) {{'Not Online'}} @else {{'Online'}} @endif</span>
 		
 	</div>
