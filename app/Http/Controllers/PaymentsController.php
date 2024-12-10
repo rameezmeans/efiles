5253843735b661c3e8e530caa8e3f3a38a1f598c
Rameez Bhatti <xrkalix@gmail.com>
2024-12-10 11:12:36 +0500

    thing

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -263,12 +263,12 @@ class PaymentsController extends Controller
 
     public function buyOffer(Request $request){
 
-        $file = TemporaryFile::findOrFail($request->file_id);
+        $file = File::findOrFail($request->file_id);
 
         $serviceCredits = $this->filesMainObj->getCredits($file);
 
-        $creditsToBuy = $serviceCredits - Auth::user()->credits->sum('credits');
-        $creditsForFile = $serviceCredits;
+        $creditsToBuy = $request->credits_to_buy;
+        $creditsForCheckout =$request->credits_for_checkout;
         $fileID = $request->file_id;
 
         $type = $request->type;
