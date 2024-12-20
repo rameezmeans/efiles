d38a1a0bcb50b3b2680b1a34b2205a2f8d040153
Rameez Bhatti <xrkalix@gmail.com>
2024-12-20 17:55:50 +0500

    test thing

--- resources/views/files/step1.blade.php
+++ resources/views/files/step1.blade.php
@@ -514,7 +514,7 @@
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-md-6">
                         @if(Auth::user()->id == 492)
-                          <button type="submit" id="register_form_Register_Popup" class="waves-effect waves-light btn btn-red" disabled>{{__('Next Test')}}</button>
+                          <button type="button" id="register_form_Register_Popup" class="waves-effect waves-light btn btn-red" disabled>{{__('Next Test')}}</button>
                         @else
                           <button type="submit" id="register_form_Register" class="waves-effect waves-light btn btn-red" disabled>{{__('Next')}}</button>
                         @endif
