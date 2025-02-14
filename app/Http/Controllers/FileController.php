e121f91f2b1cf918d71bb7ab3acef9b579aa3942
Rameez Bhatti <xrkalix@gmail.com>
2025-02-14 06:27:43 +0500

    vehicle thing

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -924,7 +924,7 @@ class FileController extends Controller
         ->where('Model', '=', $model)
         ->where('Generation', '=', $version)
         ->where('Engine', '=', $engine)
-        // ->whereNotNull('Brand_image_url')
+        // ->whereNotNull('Brand_image_url') // url_chnage
         ->first();
 
         if($vehicle){
