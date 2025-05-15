d9537812dbb5e9fff2e97c8572221b72330fdfae
Rameez Bhatti <xrkalix@gmail.com>
2025-05-15 19:08:43 +0500

    terms page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,10 +1,10 @@
 <div>
-    {!!$text!!}
-</div>
-
-<div>
     <p>Cusotmer Name:{{$name}}</p>
     <p>Customer Email:{{$email}}</p>
     <p>Customer IP:{{$ip}}</p>
     <p>Time Created:{{$time_created}}</p>
 </div>
+<div>
+    {!!$text!!}
+</div>
+
